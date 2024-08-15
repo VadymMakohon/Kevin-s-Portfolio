54d7a167601d5b60403ae4869effc3097088e5f6
VadymMakohon <errorks7@gmail.com>
2024-08-15 13:12:18 +0200

    updated email and change js

--- script.js
+++ script.js
@@ -1,4 +1,13 @@
 document.getElementById('mode-toggle').addEventListener('click', function () {
     document.body.classList.toggle('dark-mode');
     document.body.classList.toggle('light-mode');
+
+    const modeIcon = document.getElementById('mode-icon');
+    if (document.body.classList.contains('dark-mode')) {
+        modeIcon.classList.remove('fa-moon');
+        modeIcon.classList.add('fa-sun');
+    } else {
+        modeIcon.classList.remove('fa-sun');
+        modeIcon.classList.add('fa-moon');
+    }
 });
