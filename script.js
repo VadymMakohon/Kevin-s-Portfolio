694c758dfedc2c0427c228cec50c692b23d3ceff
VadymMakohon <errorks7@gmail.com>
2024-08-15 13:06:55 +0200

    added script

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById('mode-toggle').addEventListener('click', function () {
+    document.body.classList.toggle('dark-mode');
+    document.body.classList.toggle('light-mode');
+});
